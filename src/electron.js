8498624ae38c5a5fe6352690118a37de950716e6
Ali Movahedi <ali_movahedi@aol.com>
2018-09-16 09:39:24 +0430

    fix: open window and handle all event in all os

--- src/electron.js
+++ src/electron.js
@@ -4,11 +4,31 @@ import { resolve } from 'path';
 let win;
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Speedster',
+  });
 
-  // and load the index.html of the app.
   win.loadFile(resolve(__dirname, 'index.html'));
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
