df8a2c8bba1254486c5fc169367aa324c680a1d1
Ali Movahedi <ali_movahedi@aol.com>
2019-01-29 19:51:33 +0330

    feat: load data to store

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-
+import db from 'Root/db';
+import loadSetting from 'Root/actions/setting/load';
+import loadDownloads from 'Root/actions/downloads/load';
 import startAria2 from './helpers/startAria2';
 import Speedster from './Speedster';
 
@@ -13,6 +15,9 @@ import Speedster from './Speedster';
 
   try {
     await startAria2();
+
+    loadSetting(db.get('setting').value());
+    loadDownloads(db.get('downloads').value());
   } catch (e) {
     console.log('errormide,', e);
   }
