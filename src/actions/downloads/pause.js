ce374df1b2ed450d79b711093ef9c34a7817c876
Ali Movahedi <ali_movahedi@aol.com>
2019-01-30 14:51:38 +0330

    feat: pause download and store it to database

--- src/actions/downloads/pause.js
+++ src/actions/downloads/pause.js
@@ -1,21 +1,26 @@
 import types from 'Root/actions';
 import store from 'Root/store';
 import fetch from 'Root/helpers/fetch';
+import db from 'Root/db';
 
 export default async (id) => {
   const download = store.getState().downloads.find(i => i.id === id);
 
-  const res = await fetch({
+  await fetch({
     method: 'aria2.pause',
     params: [
       download.gid,
     ],
   });
 
-  console.log(res);
-
   store.dispatch({
     type: types.downloads.PAUSE,
     id,
   });
+
+  const toSave = store.getState().downloads.find(i => i.id === id);
+  db.get('downloads')
+    .find({ id })
+    .assign(toSave)
+    .write();
 };
