aa90c556e865e4a145c5aeea95b007355ef03750
Ali Movahedi <ali_movahedi@aol.com>
2019-01-31 10:33:48 +0330

    feat: ensure about speedster directory

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { ensureDir } from 'fs-extra';
+import { resolve } from 'path';
 import db from 'Root/db';
 import loadSetting from 'Root/actions/setting/load';
 import loadDownloads from 'Root/actions/downloads/load';
@@ -13,9 +15,17 @@ import Speedster from './Speedster';
   try {
     await startAria2();
 
+    const categories = db.get('categories').value();
+    const setting = db.get('setting').value();
+    const ensures = [ensureDir(resolve(setting.downloaddir, '.temp'))];
+    for (const category of categories) {
+      ensures.push(ensureDir(resolve(setting.downloaddir, category.name)));
+    }
+    await Promise.all(ensures);
+
     statusUpdater();
 
-    loadSetting(db.get('setting').value());
+    loadSetting(setting);
     loadDownloads(db.get('downloads').value());
   } catch (e) {
     history.push('/failed');
