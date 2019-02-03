dc64d1c52071139eef79a55bbe0b5adf360b6732
Ali Movahedi <ali_movahedi@aol.com>
2019-02-03 13:32:50 +0330

    feat: save changes to database

--- src/actions/queue/change.js
+++ src/actions/queue/change.js
@@ -1,9 +1,14 @@
 import types from 'Root/actions';
 import store from 'Root/store';
+import db from 'Root/db';
 
 export default (toChange) => {
   store.dispatch({
     type: types.queue.CHANGE,
     toChange,
   });
+
+  db.get('queue')
+    .assign(toChange)
+    .write();
 };
