7dc2463a5bffef84a6bb0a485d5230aef95caff1
Ali Movahedi <ali_movahedi@aol.com>
2019-02-03 20:02:23 +0330

    feat: update db for new releases

--- src/db.js
+++ src/db.js
@@ -3,15 +3,17 @@ import FileSync from 'lowdb/adapters/FileSync';
 import { resolve } from 'path';
 import { homedir } from 'os';
 import { remote } from 'electron';
+import { version } from '../package.json';
 
 const adapter = new FileSync(resolve(remote.app.getPath('appData'), 'speedster.db.json'));
 const db = lowdb(adapter);
 
-db.defaults({
+const defaults = {
   setting: {
     port: 6812,
     url: 'http://localhost:6812/jsonrpc',
     downloaddir: resolve(homedir(), 'Downloads', 'Speedster'),
+    version,
   },
   downloads: [
   ],
@@ -20,8 +22,15 @@ db.defaults({
     startTime: '03:00:00',
     endTime: '06:00:00',
   },
-}).write();
+};
+
+db.defaults(defaults).write();
 
 db.read();
+
+const setting = db.get('setting').value();
+if (setting.version !== version) {
+  db.setState(defaults).write();
+}
 
 export default db;
