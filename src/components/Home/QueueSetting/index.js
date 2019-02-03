f3b8ff6a855cefd3a17806c902c9f853dbb7912a
Ali Movahedi <ali_movahedi@aol.com>
2019-02-03 12:21:49 +0330

    feat: add Power switch button in setting

--- src/components/Home/QueueSetting/index.js
+++ src/components/Home/QueueSetting/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {
   Card,
+  Switch,
+  Row,
+  Col,
 } from 'antd';
+import { connect } from 'react-redux';
+import styles from './index.less';
 
-const Setting = () => (
-  <Card>
+const Setting = props => (
+  <Card
+    title={(
+      <div className={styles.betweenSpace}>
+        <span>
+          Queue Setting
+        </span>
+        <span>
+          Power &nbsp;
+          <Switch
+            defaultChecked={props.queue.status}
+          />
+        </span>
+      </div>
+    )}
+  >
     <p>
       Setting
     </p>
   </Card>
 );
 
-export default Setting;
+export default connect(state => ({
+  queue: state.queue,
+}))(Setting);
