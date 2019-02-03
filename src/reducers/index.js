892f11ca6d292518da4f1fe14320f8b096826eed
Ali Movahedi <ali_movahedi@aol.com>
2019-02-03 12:23:51 +0330

    feat: add queue reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import setting from './setting';
 import downloads from './downloads';
 import activeMenu from './activeMenu';
+import queue from './queue';
 
 export default combineReducers({
   setting,
   downloads,
   activeMenu,
+  queue,
 });
