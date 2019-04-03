ef4494d97b7359d6b8bb3e255b8710596e2b7a56
Ali Movahedi <ali_movahedi@aol.com>
2019-04-03 22:52:59 +0430

    feat: add auto-reloader temporarly
    fix: fix file paths in electron

--- src/electron/index.js
+++ src/electron/index.js
@@ -4,19 +4,23 @@ import { resolve } from 'path';
 import { env } from 'process';
 import { load as loadDB } from 'Root/db';
 
+// if (env.NODE_ENV === 'development') {
+//   require('electron-reload')(resolve(__dirname, '..')); // eslint-disable-line
+// }
+
 let win;
 
 function createWindow() {
   win = new BrowserWindow({
     minWidth: 1000,
     minHeight: 600,
-    icon: resolve(__dirname, '../../icons/256x256.png'),
+    icon: resolve(__dirname, '../icons/256x256.png'),
     title: 'Speedster',
     show: false,
     nodeIntegration: false,
   });
 
-  win.loadURL(`file://${resolve(__dirname, '../index.html')}`);
+  win.loadURL(`file://${resolve(__dirname, 'index.html')}`);
 
   if (env.NODE_ENV === 'development') {
     win.webContents.openDevTools();
