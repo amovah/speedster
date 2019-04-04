5728e135dd0c8ee4f0dc39a2fa871cab764a253b
Ali Movahedi <ali_movahedi@aol.com>
2019-04-04 20:35:08 +0430

    refactor: remove initializing and failed components

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { ensureDir } from 'fs-extra';
-import { resolve } from 'path';
 import history from 'Root/history';
 import categories from 'Root/categories';
 import startQueue from 'Root/actions/queue/start';
@@ -10,15 +8,9 @@ import stopQueue from 'Root/actions/queue/stop';
 import changeQueue from 'Root/actions/queue/change';
 import statusUpdater from './helpers/statusUpdater';
 import Speedster from './Speedster';
-import Init from './components/Init';
-
-render(
-  <Init />,
-  global.document.getElementById('root'),
-);
 
 (async () => {
-  try {
+  // try {
   //   changeQueue({
   //     isDownloading: false,
   //   });
@@ -28,15 +20,15 @@ render(
   //   }
   //
   //   statusUpdater();
-  } catch (e) {
-    history.push('/failed');
-    return;
-  }
+  // } catch (e) {
+  //   history.push('/failed');
+  //   return;
+  // }
   //
-  // render(
-  //   <Speedster />,
-  //   global.document.getElementById('root'),
-  // );
+  render(
+    <Speedster />,
+    global.document.getElementById('root'),
+  );
 
   if (process.env.NODE_ENV !== 'development') {
     history.push('/all');
--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Failed from './Failed';
 import Home from './Home';
 
 export default () => (
   <Switch>
-    <Route path="/failed" exact component={Failed} />
     <Route path="/" component={Home} />
   </Switch>
 );
