940d5e5f1d049404bee419bed6becc858b28c8e2
Ali Movahedi <ali_movahedi@aol.com>
2019-04-04 20:30:55 +0430

    style: remove comment

--- src/electron/index.js
+++ src/electron/index.js
@@ -46,7 +46,6 @@ if (!gotTheLock) {
   app.quit();
 } else {
   app.on('second-instance', () => {
-    // Someone tried to run a second instance, we should focus our window.
     if (win) {
       if (win.isMinimized()) {
         win.restore();
