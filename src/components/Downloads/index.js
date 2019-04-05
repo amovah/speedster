af0fb6803faaaac13a0c4dcc84eb5a2580118e36
Ali Movahedi <ali_movahedi@aol.com>
2019-04-05 10:31:55 +0430

    refactor: refactor Downlaods page

---
+++ src/components/Downloads/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import DownloadTable from 'Root/components/DownloadTable';
+
+const Queue = props => <DownloadTable downloads={props.downloads} />;
+
+const select = (state, props) => {
+  if (props.match.params.category === 'completeds') {
+    return {
+      downloads: state.downloads.filter(
+        i => i.downloadStatus === 'completed',
+      ),
+    };
+  }
+
+  if (props.match.params.category === 'incompleteds') {
+    return {
+      downloads: state.downloads.filter(
+        i => i.downloadStatus !== 'completed',
+      ),
+    };
+  }
+
+  if (
+    [
+      'compresseds',
+      'pictures',
+      'musics',
+      'videos',
+      'others',
+    ].includes(props.match.params.category)
+  ) {
+    return {
+      downloads: state.downloads.filter(
+        i => i.category.toLowerCase() === props.match.params.category,
+      ),
+    };
+  }
+
+  return {
+    downloads: state.downloads.filter(i => i.queue),
+  };
+};
+
+export default connect(select)(Queue);
--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Switch404 from 'Root/components/Switch404';
 import AddUrl from './AddUrl';
 import AppFrame from './AppFrame';
+import Downloads from './Downloads';
 
 export default () => (
-  <Switch>
-    <Route>
-      <AppFrame>
-        <Switch404>
-          <Route path="/add-url" component={AddUrl} />
-        </Switch404>
-      </AppFrame>
-    </Route>
-  </Switch>
+  <AppFrame>
+    <Switch404>
+      <Route path="/add-url" component={AddUrl} />
+      <Route path="/downloads/:category" component={Downloads} />
+    </Switch404>
+  </AppFrame>
 );
