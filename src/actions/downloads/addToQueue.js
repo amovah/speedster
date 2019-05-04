ef64cb45c2cba58eadb57a148f8ec8e30a28cdc1
Ali Movahedi <ali_movahedi@aol.com>
2019-05-04 14:23:40 +0430

    remove old addToQueue function

--- src/actions/downloads/addToQueue.js
+++
@@ -1,7 +0,0 @@
-import quietAdd from './quietAdd';
-import moveToQueue from './moveToQueue';
-
-export default async (downloadInfo, details) => {
-  const id = await quietAdd(downloadInfo, details);
-  moveToQueue(id);
-};
