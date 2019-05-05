82dd0cbc9936863168c9f020a5fcd401fea87b44
Ali Movahedi <ali_movahedi@aol.com>
2019-05-05 12:15:28 +0430

    remove useless files

--- src/actions/downloads/removeFromQueue/remove.js
+++
@@ -1,7 +0,0 @@
-import update from '../update';
-
-export default async (id) => {
-  update(id, {
-    queue: false,
-  });
-};
--- src/actions/downloads/removeFromQueue/single.js
+++
@@ -1,8 +0,0 @@
-import { sync } from 'Root/db';
-import remove from './remove';
-
-export default async (id) => {
-  remove(id);
-
-  await sync();
-};
