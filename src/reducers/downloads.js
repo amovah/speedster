76d5bd9bcd0be1ddb8883491a74c47234d9a5a60
Ali Movahedi <ali_movahedi@aol.com>
2019-05-06 11:30:30 +0430

    remove old pause reducer handler in downloads reducer

--- src/reducers/downloads.js
+++ src/reducers/downloads.js
@@ -45,40 +45,6 @@ export default (state = [], action) => {
       }));
     }
 
-    case types.downloads.PAUSE: {
-      const index = state.findIndex(item => item.id === action.id);
-      return [
-        ...state.slice(0, index),
-        {
-          ...state[index],
-          downloadStatus: 'pause',
-        },
-        ...state.slice(index + 1),
-      ];
-    }
-
-    case types.downloads.PAUSE_ALL: {
-      return state.map((item) => {
-        const res = { ...item };
-        if (res.downloadStatus === 'downloading') {
-          res.downloadStatus = 'pause';
-        }
-
-        return res;
-      });
-    }
-
-    case types.downloads.BULK_PAUSE: {
-      return state.map((item) => {
-        const res = { ...item };
-        if (action.ids.includes(res.id) && res.downloadStatus === 'downloading') {
-          res.downloadStatus = 'pause';
-        }
-
-        return res;
-      });
-    }
-
     case types.downloads.RESUME: {
       const index = state.findIndex(item => item.id === action.id);
       return [
