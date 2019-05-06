9076d35376f22ff6e4c08a5a3609ff0501223a46
Ali Movahedi <ali_movahedi@aol.com>
2019-05-06 11:30:02 +0430

    refactor: refactor pauseAll, use bulkUpdate

--- src/actions/downloads/pause/pauseAll.js
+++ src/actions/downloads/pause/pauseAll.js
@@ -1,16 +1,19 @@
-import types from 'Root/actions';
 import store from 'Root/store';
 import { sync } from 'Root/db';
 import fetch from 'Root/helpers/fetch';
+import bulkUpdate from '../update/bulk';
 
 export default async () => {
-  store.dispatch({
-    type: types.downloads.PAUSE_ALL,
-  });
-
   await fetch({
     method: 'aria2.pauseAll',
   });
+
+  bulkUpdate(
+    store.getState().downloads.filter(i => i.downloadStatus === 'downloading').map(i => i.id),
+    {
+      downloadStatus: 'pause',
+    },
+  );
 
   await sync();
 };
