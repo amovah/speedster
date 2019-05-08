b357b2ea37129a9fd565feb4858ba39d7b0c45b0
Ali Movahedi <ali_movahedi@aol.com>
2019-05-08 11:13:55 +0430

    cleanup shutdown script

--- src/helpers/shutdown.js
+++ src/helpers/shutdown.js
@@ -1,19 +1,7 @@
-import { remote } from 'electron';
 import fetch from 'Root/helpers/fetch';
-import store from 'Root/store';
-// import suspend from 'Root/actions/downloads/suspend';
 
 export default async () => {
-//   const downloadings = store.getState().downloads
-//     .filter(i => ['downloading', 'pause'].includes(i.downloadStatus))
-//     .map(async (i) => { await suspend(i.id); });
-//   await Promise.all(downloadings);
-
   await fetch({
     method: 'aria2.shutdown',
   });
-
-  // if (process.env.NODE_ENV !== 'development') {
-  //   remote.app.quit();
-  // }
 };
